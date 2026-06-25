fd18587aa44aff8ed3f30cbe472a434a5cb02d11
Karthik Naresh Panyam <85242011+KarthikPanyam@users.noreply.github.com>
2026-06-24 22:03:21 -0400

    Update fifortl.v

--- Verilog/lab3/rtl/fifortl.v
+++ Verilog/lab3/rtl/fifortl.v
@@ -1,88 +1,48 @@
-module fifortl(
-			input clk, rst, we, re,
-			input [7:0] din,
-			output reg [7:0] dout,
-			output empty, full);
-			
-			
-			
-	reg [4:0] wrptr, rdptr;
-	reg [7:0] mem [0:15];
-	
-	
-	integer i;
-	
-	
-	assign full = ((wrptr[4] != rdptr[4] ) && (wrptr[3:0] == rdptr[3:0]) ) ? 1'b1: 1'b0;
-	assign empty = (rdptr == wrptr)? 1'b1: 1'b0;
-	
-	// Write operation
-	
-	always @ (posedge clk) 
-		begin
-		
-			if (rst) begin
-			
-				for(i=0;i<16;i = i+1) begin
-					mem[i] <= 0;
-				end
-				wrptr <= 0;
-				
-			
-			end
-		
-		
-			else if ( we && !full) begin
-				mem[wrptr[3:0]] <= din;
-				wrptr <= wrptr + 1;
-			end
-		
-		
-			else 
-				wrptr<=wrptr;
-			
-		end
-		
-		
-	// end of Write operation
-	
-	
-	
-	
-	// Read operation
-	
-	
-	always @ ( posedge clk)
-		begin
-		
-			if (rst) begin
-			rdptr<=0;
-			dout<=0;
-			
-			end
-			
-			
-			
-			else if ( re && !empty) begin
-				dout <= mem[rdptr[3:0]];
-				rdptr <= rdptr + 1;
-				
-			end
-			
-			else begin
-			
-				dout<= dout;
-				rdptr <= rdptr;
-				
-			end
-			
-		end
-		
-		
-	// End of Read operation
-	
-	
-	
+module fifo16x8 (
+input clk, rst,
+input [7:0] din,
+input re, we,
+output reg [7:0] dout,
+output empty, full);
 
-	
+reg [4:0] wr_ptr, rd_ptr;
+
+integer i;
+
+reg [7:0] mem [0:15];
+
+always @ (posedge clk)
+begin
+
+	if(rst) begin
+		dout<=0;
+		{wr_ptr, rd_ptr} <= 0;
+		for (i = 0; i<16; i = i+1)
+			mem[i] <= 0;
+	end
+
+
+	else begin
+
+		if(we && !full) begin
+
+			mem[wr_ptr[3:0]] <= din;
+			wr_ptr <= wr_ptr + 1;
+		end
+
+		if(re && !empty) begin
+
+			dout<= mem[rd_ptr[3:0]];
+			rd_ptr <= rd_ptr + 1;
+		end
+	end
+
+
+end
+
+assign empty = ( wr_ptr == rd_ptr) ? 1'b1:1'b0;
+
+assign full = ( (wr_ptr[3:0] == rd_ptr[3:0]) && (wr_ptr[4] != rd_ptr[4]) ) ? 1'b1:1'b0;
+
+
 endmodule
